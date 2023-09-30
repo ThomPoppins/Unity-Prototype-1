7b8fb5e6cdf85502accade03b6c09e55f1d8f7b4
Thom Poppins <thompoppins@gmail.com>
2023-09-30 12:32:49 +0100

    Turn vehicle left and right based on user input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal"); // This is a value between -1 and 1
+
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
