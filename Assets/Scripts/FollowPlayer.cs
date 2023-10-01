e0ef1dcd270dc19f49b29a0619e152178be96dbd
Thom Poppins <thompoppins@gmail.com>
2023-10-01 13:28:04 +0100

    Toggle first person view

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
+    private bool firstPerson = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,28 @@ public class FollowPlayer : MonoBehaviour
     // LateUpdate is called after Update() has been called once per frame
     void LateUpdate()
     {
+        // Switch offset if c button is pressed
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            firstPerson = !firstPerson;
+        }
+
+        if (firstPerson)
+        {
+            // Set camera to player's position
+            offset = new Vector3(0, 1.76f, 1.33f);
+            // Rotate camera to player's rotation
+            transform.rotation = player.transform.rotation;
+        }
+        else
+        {
+            // Offset the camera behind the player by adding to the player's position
+            offset = new Vector3(0, 5, -7);
+            // Stop rotating camera to player's rotation
+            transform.rotation = Quaternion.Euler(30, 0, 0);
+        };
+
+        // Offset the camera behind the player by adding to the player's position
         transform.position = player.transform.position + offset;
     }
 }
