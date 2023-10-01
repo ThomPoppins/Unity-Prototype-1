ed6d2d2c801c06275697a7d3e7b91f494e5beba9
Thom Poppins <thompoppins@gmail.com>
2023-10-01 09:46:15 +0100

    Make the propeller spin

--- Assets/Challenge 1/Scripts/MovePropellerX.cs
+++ Assets/Challenge 1/Scripts/MovePropellerX.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class MovePropellerX : MonoBehaviour
 {
-    public GameObject propeller;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Make the propeller spin
+        transform.Rotate(Vector3.forward * 500 * Time.deltaTime);
     }
 }
